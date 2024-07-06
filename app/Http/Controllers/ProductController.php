163b6f40f6f10959f4c18e19b17d833a61ec31e7
mrizkystr <muhamadrizkysatria23@gmail.com>
2024-07-06 15:52:21 +0700

    Perubahan dikit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,25 +2,40 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Merchant; // Pastikan model Merchant diimpor
+use App\Http\Resources\ProductResource;
+use App\Http\Resources\MerchantResource;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
+    /**
+     * Display a listing of the products.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
-        return Product::all();
+        $products = Product::with('merchant')->get();
+        return ProductResource::collection($products);
     }
 
+    /**
+     * Store a newly created product in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function store(Request $request)
     {
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
             'merchants_id' => 'required|integer',
-            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048', // Validasi gambar
             'price' => 'required|string',
             'status' => 'required|in:active,inactive',
+            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
         ]);
 
         if ($validator->fails()) {
@@ -37,14 +52,31 @@ class ProductController extends Controller
 
         $product = Product::create($data);
 
-        return response()->json($product, 201);
+        // Memuat relasi merchant
+        $product->load('merchant');
+
+        return new ProductResource($product);
     }
 
+    /**
+     * Display the specified product.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function show($id)
     {
-        return Product::findOrFail($id);
+        $product = Product::with('merchant')->findOrFail($id);
+        return new ProductResource($product);
     }
 
+    /**
+     * Update the specified product in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function update(Request $request, $id)
     {
         $product = Product::findOrFail($id);
@@ -52,9 +84,9 @@ class ProductController extends Controller
         $validator = Validator::make($request->all(), [
             'name' => 'sometimes|required|string|max:255',
             'merchants_id' => 'sometimes|required|integer',
-            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048', // Validasi gambar
             'price' => 'sometimes|required|string',
             'status' => 'sometimes|required|in:active,inactive',
+            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
         ]);
 
         if ($validator->fails()) {
@@ -71,9 +103,18 @@ class ProductController extends Controller
 
         $product->update($data);
 
-        return response()->json($product);
+        // Memuat relasi merchant setelah update
+        $product->load('merchant');
+
+        return new ProductResource($product);
     }
 
+    /**
+     * Remove the specified product from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function destroy($id)
     {
         $product = Product::findOrFail($id);
@@ -82,3 +123,4 @@ class ProductController extends Controller
         return response()->json(null, 204);
     }
 }
+
