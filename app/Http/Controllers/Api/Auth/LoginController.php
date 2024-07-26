be07c01374f9dd74b25187d57b575448ea3b1a46
mrizkystr <muhamadrizkysatria23@gmail.com>
2024-07-26 09:15:36 +0700

    update-login

--- app/Http/Controllers/Api/Auth/LoginController.php
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -10,39 +10,41 @@ use Tymon\JWTAuth\Facades\JWTAuth;
 class LoginController extends Controller
 {
     public function index(Request $request)
-    {
-        //set validasi
-        $validator = Validator::make($request->all(),[
-            'email'     => 'required|email',
-            'password'  => 'required',
-        ]);
+{
+    //set validasi
+    $validator = Validator::make($request->all(),[
+        'email'     => 'required|email',
+        'password'  => 'required',
+    ]);
 
-        //response error validasi
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), 422);
-        }
-
-        //get "email" dan "password" dari input
-        $credentials = $request->only('email', 'password');
-
-        //check jika "email" dan "password" tidak sesuai
-        if(!$token = auth()->guard('api')->attempt($credentials))
-            return response()->json([
-                'success' => false,
-                'message' => 'Email atau password salah'
-            ], 400);
-
-        $user = auth()->guard('api')->user();
-
-        // response login "success" dengan generate "Token"
-        return response()->json([
-            'success'       => true,
-            'user'          => $user->only(['username', 'email', 'role']),
-            'permissions'   => [], // Replace with actual permissions if applicable
-            'role'          => $user->role,
-            'token'         => $token
-        ], 200);
+    //response error validasi
+    if ($validator->fails()) {
+        return response()->json($validator->errors(), 422);
     }
+
+    //get "email" dan "password" dari input
+    $credentials = $request->only('email', 'password');
+
+    //check jika "email" dan "password" tidak sesuai
+    if(!$token = auth()->guard('api')->attempt($credentials))
+        return response()->json([
+            'success' => false,
+            'message' => 'Email atau password salah'
+        ], 400);
+
+    $user = auth()->guard('api')->user();
+
+    // response login "success" dengan generate "Token"
+    return response()->json([
+        'success'       => true,
+        'user_id'       => $user->id,
+        'user'          => $user->only(['username', 'email', 'role']),
+        'permissions'   => [], // Replace with actual permissions if applicable
+        'role'          => $user->role,
+        'token'         => $token
+    ], 200);
+}
+
 
     public function logout()
     {
